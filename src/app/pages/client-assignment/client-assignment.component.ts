140951d0fb5c5f13bd359429baff0ed1a8677429
Ahmad Aminullah <ahmad.aminullah@ui.ac.id>
2024-05-20 17:41:27 +0700

    fix: finish connect client assignment to backend

--- src/app/pages/client-assignment/client-assignment.component.ts
+++ src/app/pages/client-assignment/client-assignment.component.ts
@@ -5,6 +5,8 @@ import { UserResDto } from "../../dto/user/user.res.dto";
 import { ClientDropdownResDto } from "../../dto/user/client-dropdown.res.dto";
 import { ConfirmationService, MessageService } from "primeng/api";
 import { NonNullableFormBuilder, Validators } from "@angular/forms";
+import { ClientAssignmentReqDto } from "../../dto/client-assignment/client-assignment.req.dto";
+import { ClientAssignmentService } from "../../services/client-assignment.service";
 
 @Component({
     selector: 'client-assignment',
@@ -19,10 +21,15 @@ export class ClientAssignment implements OnInit {
         private userService: UserService, 
         private confirmationService: ConfirmationService, 
         private messageService: MessageService,
-        private fb: NonNullableFormBuilder
+        private fb: NonNullableFormBuilder,
+        private clientAssignmentService: ClientAssignmentService
     ){}
 
     ngOnInit(): void {
+        this.init()
+    }
+
+    init(): void {
         firstValueFrom(this.userService.getAllPs()).then(
             res => {
                 this.payrollServices = res
@@ -50,11 +57,22 @@ export class ClientAssignment implements OnInit {
             rejectIcon:"none",
             rejectButtonStyleClass:"p-button-text",
             accept: () => {
+                this.onSubmit()
                 this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
             },
             reject: () => {
                 this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
             }
         });
+    }
+
+    onSubmit() {
+        if (this.clientAssignmentReqDtoFormGroup.valid) {
+            const clientAssignmentReqDto: ClientAssignmentReqDto = this.clientAssignmentReqDtoFormGroup.getRawValue()
+            console.log(clientAssignmentReqDto)
+            firstValueFrom(this.clientAssignmentService.save(clientAssignmentReqDto)).then(res =>
+                this.clientAssignmentReqDtoFormGroup.reset()
+            )
+        }
     }
 }

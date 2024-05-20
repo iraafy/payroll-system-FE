a6c91be678103f92b13ae2abb87bed1bf450a493
torotoro21 <anggaprihantoro3@gmail.com>
2024-05-20 13:52:27 +0700

    fix create company

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from "@angular/core";
 import { BaseService } from "./base.service";
+import { CompanyReqDto } from "../dto/company/company.req.dto";
+import { InsertResDto } from "../dto/insert.res.dto";
 
 @Injectable({
     providedIn: 'root'
 })
 export class CompanyService {
 
-    constructor(private baseService : BaseService) { }
+    constructor(private baseService: BaseService) { }
 
     getAllCompany() {
         return this.baseService.get<any>('companies')
     }
-    
+
+    createNewCompany(data: CompanyReqDto) {
+        return this.baseService.post<InsertResDto>('companies/new', data)
+    }
+
 }

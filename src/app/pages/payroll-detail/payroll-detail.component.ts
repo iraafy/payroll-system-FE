9e2c310e926f00249bf07348d2bcd3757ca65c5f
r3xxxb3 <reybernard1511@gmail.com>
2024-06-03 14:46:49 +0700

    feat: signature pad on payroll detail

--- src/app/pages/payroll-detail/payroll-detail.component.ts
+++ src/app/pages/payroll-detail/payroll-detail.component.ts
@@ -1,24 +1,23 @@
-import { AfterViewInit, Component, OnInit } from "@angular/core";
-import { AuthService } from "../../services/auth.service";
-import { RoleType } from "../../constants/role-type";
-import { ActivatedRoute, Route, Router } from "@angular/router";
-import { Observable, firstValueFrom, tap } from "rxjs";
-import { PayrollService } from "../../services/payroll.service";
-import { PayrollDetailResDto } from "../../dto/payroll-detail/payroll-detail.res.dto";
-import { DatePipe } from "@angular/common";
-import { NonNullableFormBuilder, Validators } from "@angular/forms";
-import { ReschduleService } from "../../services/reschedule.service";
-import { MessageService } from "primeng/api";
-import { PayrollResDto } from "../../dto/payroll/payroll.res.dto";
-import { NotificationReqDto } from "../../dto/notification/notification.req.dto";
-import { NotificationService } from "../../services/notification.service";
-import { BASE_URL } from "../../constants/global";
-import { FileService } from "../../services/file.service";
-import { FtpReqDto } from "../../dto/ftp/ftp.req.dto";
-import SignaturePad from "signature_pad";
-import { HttpResponse } from "@angular/common/http";
-import { PDFDocumentProxy } from "pdfjs-dist";
-import { PdfService } from "../../services/pdf.service";
+import { AfterViewInit, Component, OnInit } from "@angular/core"
+import { AuthService } from "../../services/auth.service"
+import { RoleType } from "../../constants/role-type"
+import { ActivatedRoute, Route, Router } from "@angular/router"
+import { Observable, firstValueFrom, tap } from "rxjs"
+import { PayrollService } from "../../services/payroll.service"
+import { PayrollDetailResDto } from "../../dto/payroll-detail/payroll-detail.res.dto"
+import { DatePipe } from "@angular/common"
+import { FormGroup, NonNullableFormBuilder, Validators } from "@angular/forms"
+import { ReschduleService } from "../../services/reschedule.service"
+import { MessageService } from "primeng/api"
+import { PayrollResDto } from "../../dto/payroll/payroll.res.dto"
+import { NotificationReqDto } from "../../dto/notification/notification.req.dto"
+import { NotificationService } from "../../services/notification.service"
+import { FileService } from "../../services/file.service"
+import SignaturePad from "signature_pad"
+import { HttpResponse } from "@angular/common/http"
+import { PDFDocumentProxy } from "pdfjs-dist"
+import { PdfService } from "../../services/pdf.service"
+import { SignatureReqDto } from "../../dto/payroll-detail/signature.req.dto"
 
 @Component({
     selector: 'payroll-detail',
@@ -26,21 +25,26 @@ import { PdfService } from "../../services/pdf.service";
 })
 
 export class PayrollDetail implements OnInit {
-    signatureVisible: boolean = false;
-    rescheduleVisible: boolean = false;
-    pingVisible: boolean = false;
-    downloadVisible: boolean = false;
-    payrollId: string | null = '';
-    clientId: string | null = '';
-    payrollDetails?: Observable<PayrollDetailResDto[]>;
-    payrolls?: PayrollResDto;
+    confirmSignVisible : boolean = false
+    signatureVisible: boolean = false
+    rescheduleVisible: boolean = false
+    pingVisible: boolean = false
+    downloadVisible: boolean = false
+    payrollId: string | null = ''
+    clientId: string | null = ''
+    payrollDetails?: Observable<PayrollDetailResDto[]>
+    payrolls?: PayrollResDto
     payrollLoop = [1]
-    companyLogos: string[] = [];
-    data: NotificationReqDto | null = null;
-    visible: boolean = false;
+    companyLogos: string[] = []
+    data: NotificationReqDto | null = null
+    visible: boolean = false
     images: string[] = []
     pages : number[] = []
     uploadedFile? : HttpResponse<HttpResponse<Blob>>
+    pickedDetailId? : string
+    signature : SignatureReqDto = {
+        signatureBase64 : ''
+    }
 
     tempTest: PayrollDetailResDto[] = []
 
@@ -70,29 +74,29 @@ export class PayrollDetail implements OnInit {
     ) { }
 
     ngOnInit(): void {
-        this.init();
+        this.init()
     }
 
     init(): void {
-        this.payrollId = this.activeRoute.snapshot.paramMap.get('id');
+        this.payrollId = this.activeRoute.snapshot.paramMap.get('id')
         if (this.payrollId != null) {
 
             firstValueFrom(this.payrollService.getPayrollById(this.payrollId)).then(
                 res => {
-                    this.payrolls = res;
-                    this.clientId = res.clientId;
+                    this.payrolls = res
+                    this.clientId = res.clientId
                 }
             )
             this.payrollDetails = this.payrollService.getAllPayrollDetailByPayrollId(this.payrollId)
                 .pipe(
                     tap((items: PayrollDetailResDto[]) => {
                         items.forEach((item) => {
-                            const formattedDate = this.datePipe.transform(item.maxUploadDate, 'yyyy-MM-dd')!;
-                            item.maxUploadDate = formattedDate;
-                        });
+                            const formattedDate = this.datePipe.transform(item.maxUploadDate, 'yyyy-MM-dd')!
+                            item.maxUploadDate = formattedDate
+                        })
                         // console.log(items)
                     })
-                );
+                )
         }
     }
 
@@ -106,43 +110,44 @@ export class PayrollDetail implements OnInit {
         }
     }
     
-    loginData = this.authService.getLoginData();
+    loginData = this.authService.getLoginData()
 
     get isPS() {
-        return this.loginData?.roleCode == RoleType.PS;
+        return this.loginData?.roleCode == RoleType.PS
     }
 
     get isClient() {
-        return this.loginData?.roleCode == RoleType.CLIENT;
+        return this.loginData?.roleCode == RoleType.CLIENT
     }
 
-    showDialogSignature() {
-        this.signatureVisible = true;
+    showDialogSignature(id : string) {
+        this.signatureVisible = true
+        this.pickedDetailId = id
     }
 
     showDialogReschedule(id: string) {
-        this.rescheduleVisible = true;
-        this.rescheduleReqDtoFg.get('payrollDetailId')?.patchValue(id);
+        this.rescheduleVisible = true
+        this.rescheduleReqDtoFg.get('payrollDetailId')?.patchValue(id)
     }
 
     showDialogPing() {
-        this.pingVisible = true;
+        this.pingVisible = true
     }
 
     onSubmitCreateReschdule() {
         if (this.rescheduleReqDtoFg.valid) {
-            const rescheduleDto = this.rescheduleReqDtoFg.getRawValue();
+            const rescheduleDto = this.rescheduleReqDtoFg.getRawValue()
 
             firstValueFrom(this.reschduleService.createNewReschdule(rescheduleDto)).then(
                 res => {
-                    this.messageService.add({ severity: 'success', summary: 'Success', detail: res.message });
-                    this.init();
+                    this.messageService.add({ severity: 'success', summary: 'Success', detail: res.message })
+                    this.init()
                 },
                 err => {
-                    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'harap cek tanggal reschedule dan approval reschedule' });
+                    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'harap cek tanggal reschedule dan approval reschedule' })
                 }
             )
-            this.rescheduleVisible = false;
+            this.rescheduleVisible = false
         }
     }
 
@@ -165,43 +170,43 @@ export class PayrollDetail implements OnInit {
 
     fileUpload(event: any, id: string) {
         const toBase64 = (file: File) => new Promise<string>((resolve, reject) => {
-            const reader = new FileReader();
-            reader.readAsDataURL(file);
+            const reader = new FileReader()
+            reader.readAsDataURL(file)
             reader.onload = () => {
-                if (typeof reader.result === "string") resolve(reader.result);
-            };
-            reader.onerror = error => reject(error);
-        });
+                if (typeof reader.result === "string") resolve(reader.result)
+            }
+            reader.onerror = error => reject(error)
+        })
 
-        const files: File[] = event.files;
+        const files: File[] = event.files
 
         for (let file of files) {
             toBase64(file).then(result => {
-                const resultBase64 = result.substring(result.indexOf(",") + 1, result.length);
-                const resultExtension = file.name.substring(file.name.lastIndexOf(".") + 1, file.name.length);
+                const resultBase64 = result.substring(result.indexOf(",") + 1, result.length)
+                const resultExtension = file.name.substring(file.name.lastIndexOf(".") + 1, file.name.length)
 
-                this.ftpReqDtoFg.get('fileContent')?.patchValue(resultBase64);
-                this.ftpReqDtoFg.get('fileExt')?.patchValue(resultExtension);
-                this.ftpReqDtoFg.get('detailId')?.patchValue(id);
+                this.ftpReqDtoFg.get('fileContent')?.patchValue(resultBase64)
+                this.ftpReqDtoFg.get('fileExt')?.patchValue(resultExtension)
+                this.ftpReqDtoFg.get('detailId')?.patchValue(id)
 
                 if (this.ftpReqDtoFg.valid) {
-                    const newFile = this.ftpReqDtoFg.getRawValue();
+                    const newFile = this.ftpReqDtoFg.getRawValue()
                     firstValueFrom(this.fileService.uploadFileFtp(newFile)).then(
                         res => {
-                            this.messageService.add({ severity: 'success', summary: 'Success', detail: res.message });
-                            firstValueFrom(this.payrollService.setPayrollDetailFile(id, res.id));
-                            this.init();
+                            this.messageService.add({ severity: 'success', summary: 'Success', detail: res.message })
+                            firstValueFrom(this.payrollService.setPayrollDetailFile(id, res.id))
+                            this.init()
                         }
 
                     )
                 }
-            });
+            })
         }
     }
 
     downloadFileSubmit(fileName: string) {
-        window.location.href = `http://localhost:8080/files/ftp/${fileName}`;
-        this.downloadVisible = false;
+        window.location.href = `http://localhost:8080/files/ftp/${fileName}`
+        this.downloadVisible = false
     }
 
     async onPreview(id: string) {
@@ -231,6 +236,27 @@ export class PayrollDetail implements OnInit {
     }
 
     showDialog() {
-        this.visible = true;
+        this.visible = true
+    }
+
+    sign(data : FormGroup) {
+        this.signature.signatureBase64 = data.get('signatureContent')?.getRawValue()
+        this.confirmSignVisible = true
+    }
+
+    submitSign() {
+        if(this.pickedDetailId){
+            firstValueFrom(this.payrollService.setSignatureOnPayrollDetail(this.pickedDetailId, this.signature)).then(
+                res => {
+                    if(res.ver) {
+                        console.log(res.ver)
+                        this.messageService.add({severity: 'success', summary:'success', detail: res.message})
+                    }
+                },
+                err => {
+                    
+                }
+            )
+        }
     }
 }

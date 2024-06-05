3e69f32e214a53e4fd221f9e3e2940c60a7edc0c
r3xxxb3 <reybernard1511@gmail.com>
2024-06-05 20:27:22 +0700

    patch: add phone to user add form

--- src/app/pages/users/user-add/user-add.component.ts
+++ src/app/pages/users/user-add/user-add.component.ts
@@ -22,6 +22,7 @@ export class UserAdd implements OnInit {
             Validators.email
         ]],
         fullName: ['', [Validators.required, this.noWhitespaceValidator]],
+        phone: ['', [Validators.required]],
         roleId: ['', [Validators.required]],
         companyId: ['', [Validators.required]],
         fileContent: [''],
@@ -91,6 +92,7 @@ export class UserAdd implements OnInit {
 
     createUser() {
         const userReq: UserReqDto = this.userForm.getRawValue()
+        console.log(this.userForm.get('phone')?.getRawValue())
         firstValueFrom(this.userService.saveUser(userReq)).then(
             res => {
                 this.messageService.add({ severity: 'success', summary: 'Berhasil', detail: 'Berhasil Menambahkan user' })

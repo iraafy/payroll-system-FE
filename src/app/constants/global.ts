ee95bb7f0c1dc79fc6d35b67c831de6a5ef60a07
iraafy <irafitriyani62@gmail.com>
2024-06-06 21:46:42 +0700

    create default title payroll

--- src/app/constants/global.ts
+++ src/app/constants/global.ts
@@ -1,1 +1,16 @@
 export const BASE_URL = 'http://localhost:8080'
+
+export const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember"
+]
